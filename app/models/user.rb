20b65cc28129c98dca42655088d08842277ead38
kzjasta <kzjasta@gmail.com>
2017-01-26 22:18:06 +0000

    Added 'Remember me' functionality for persistent sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  attr_accessor :remember_token
+
+  #User Attributes and Validation
   before_save {self.email = email.downcase}
   validates :username,   presence: true, length:{maximum:15},
                          uniqueness: {case_sensitive: false}
@@ -13,4 +17,34 @@ class User < ApplicationRecord
                          
   has_secure_password
   validates :password, length: {minimum: 7, maximum: 14}
+
+  #Returns hash digest of a string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  #Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #Remembers a user in the database to make a session persistent
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  #Returns true if token matches digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? (remember_token)
+  end
 end
