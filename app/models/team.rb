a33e40bd690c81a25591acb83b33470693d9b9ed
kzjasta <kzjasta@gmail.com>
2017-02-02 00:03:22 +0000

    Fixed team saving bug

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,6 @@ class Team < ApplicationRecord
   has_many :players, dependent: :destroy
 
   #Team Attributes and Validation
-  validates :team_id, presence: true
   validates :name, presence: true, length:{maximum: 40}
   validates :description, presence: true, length:{maximum: 140}
 end
