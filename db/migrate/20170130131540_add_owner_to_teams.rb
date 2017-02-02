95090845925a074148998ce7601137028c75b566
kzjasta <kzjasta@gmail.com>
2017-02-02 01:03:43 +0000

    Fixed migration conflicts

--- db/migrate/20170130131540_add_owner_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerToTeams < ActiveRecord::Migration[5.0]
-  def change
-    add_column :teams, :owner, :string
-  end
-end
--- db/migrate/20170201130204_drop_players.rb
+++
@@ -1,5 +0,0 @@
-class DropPlayers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :players
-  end
-end
