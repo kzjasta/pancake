388a1b2e3988bffbbf5c5c9184f108265832156c
kzjasta <kzjasta@gmail.com>
2017-03-01 17:06:53 +0000

    Styling landing page

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -4,6 +4,7 @@ class Fixture < ApplicationRecord
   belongs_to :venue, :class_name => "Venue", foreign_key: "venue_id"
   belongs_to :home_team, :class_name => "Team",  foreign_key: "home_team_id"
   belongs_to :away_team, :class_name => "Team", foreign_key: "away_team_id"
+  has_many :match_sets, dependent: :destroy
 
 
   #Fixture Attributes and Validations
